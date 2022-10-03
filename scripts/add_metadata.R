8da6fb6bb70c87e61cc4f2966f84f7b8806b239c
Shahroze Abbas <28969387+shahrozeabbas@users.noreply.github.com>
2022-10-02 23:42:01 -0400

    Update add_metadata.R

--- scripts/add_metadata.R
+++ scripts/add_metadata.R
@@ -4,22 +4,21 @@ add_metadata <- function(object, protospacers=NULL, scrub_doublets=TRUE) {
     guides <- fread(protospacers)[num_features == 1]
     guides[, target := tstrsplit(feature_call, '_')[1]]
 
-    # add guides
-    g <- guides[, fifelse(feature_call %like% 'NEG', 'NT', feature_call)]
-    names(g) <- guides[, cell_barcode]
-
     # add targets
     t <- guides[, target]
-    names(t) <- guides[, cell_barcode]
+    
+    # add NT
+    nt <- guides[, fifelse(target %chin% 'NEG', 'NT', target)]
+    
+    # add guides
+    g <- guides[, fifelse(feature_call %like% 'NEG', 'NT', feature_call)]
 
     # add status
     s <- guides[, fifelse(target %chin% 'NEG', 'non-targeting', 'perturbed')]
-    names(s) <- guides[, cell_barcode]
-
-    # add NT
-    nt <- guides[, fifelse(target %chin% 'NEG', 'NT', target)]
-    names(nt) <- guides[, cell_barcode]
-
+    
+    names(g) <- names(t) <- names(s) <- names(nt) <- guides[, cell_barcode]
+    
+    
     object <- object %>% 
                 subset(cells=guides[, cell_barcode]) %>% 
 
